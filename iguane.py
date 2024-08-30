8d83546ae1b67492080455ec50a14d54f6456e90
Olexa Bilaniuk <olexa.bilaniuk@mila.quebec>
2024-08-30 15:57:26 -0400

    Refactor slightly the UGR versioning.

--- iguane.py
+++ iguane.py
@@ -179,26 +179,27 @@ RAWDATA = {            #             TFLOPS          TFLOPS          TFLOPS     
 }
 
 
-VERSIONS = {
+UGR_VERSIONS = {
     # v1.0 is equivalent to UGR
     # https://docs.alliancecan.ca/wiki/Allocations_and_compute_scheduling
-    1.0: { 'fp16': 1.6, 'fp32': 1.6, 'tf32': 0.0, 'memgb': 0.8, 'membw': 0.0 },
+    "1.0":        { 'fp16': 1.6, 'fp32': 1.6, 'memgb': 0.8 },
+    "1.0-renorm": { 'fp16': 0.4, 'fp32': 0.4, 'memgb': 0.2 },
 }
 
 
 @fom
-def fom_ugr(name, **kwargs):
-    data = RAWDATA[name].copy()
-    data['fp16'] = data['fp16'] or data['fp32']
+def fom_ugr(name, *, args=None):
+    weights = UGR_VERSIONS[args.ugr_version if args else "1.0"]
     ref  = RAWDATA['A100-SXM4-40GB']
-    return 1.6 * (data['fp16']  / ref['fp16']) + \
-           1.6 * (data['fp32']  / ref['fp32']) + \
-           0.8 * (data['memgb'] / ref['memgb'])
+    data = RAWDATA[name].copy()
+    data['tf32'] = data['tf32'] or data['fp32']
+    data['fp16'] = data['fp16'] or data['fp32']
+    return sum([w * (data[k] / ref[k]) for k, w in weights.items()])
 
 @fom
 def fom_iguane(name, *, args=None):
-    data = RAWDATA[name].copy()
     ref  = RAWDATA['A100-SXM4-80GB']
+    data = RAWDATA[name].copy()
     data['tf32'] = data['tf32'] or data['fp32']
     data['fp16'] = data['fp16'] or data['fp32']
     weight_fp16  = 0.2
@@ -212,18 +213,6 @@ def fom_iguane(name, *, args=None):
            weight_memgb * (data['memgb'] / ref['memgb']) + \
            weight_membw * (data['membw'] / ref['membw'])
 
-@fom
-def fom_version(name, *, args=None):
-    data = RAWDATA[name].copy()
-    ref  = RAWDATA['A100-SXM4-40GB']
-    data['tf32'] = data['tf32'] or data['fp32']
-    weights = VERSIONS[args.version]
-    return sum(
-        [
-            weight * data[criteria] / ref[criteria]
-            for criteria, weight in weights.items()
-        ]
-    )
 
 
 if __name__ == "__main__":
@@ -234,6 +223,9 @@ if __name__ == "__main__":
                       help="Sort GPU listing by unit")
     argp.add_argument('--list-units',      action='store_true',
                       help="List known Units")
+    argp.add_argument('--list-ugr-versions', '--list-rgu-versions',
+                      action='store_true',
+                      help="List known UGR/RGU versions")
     argp.add_argument('--list-gpus', '-l', action='store_true',
                       help="List known GPUs")
     argp.add_argument('--gpu',  '-G', type=str, default=None,
@@ -250,8 +242,8 @@ if __name__ == "__main__":
     
     ugrp = argp.add_argument_group('UNITS', 'Unit/FoM selection')
     umtx = ugrp.add_mutually_exclusive_group()
-    umtx.add_argument('--unit', '-u', '--fom', type=str.lower, default='iguane',
-                      choices={'iguane', 'iguana', 'rgu', 'ugr'},
+    umtx.add_argument('--unit', '-u', '--fom', type=str.lower, default='ugr',
+                      choices=sorted(set(FOM.keys()) | {'iguana', 'rgu'}),
                       help="Select unit-equivalence (FoM) to compute for every GPU")
     umtx.add_argument('--iguane',  action='store_const', dest='unit', const='iguane',
                       help="Select IGUANE/IGUANA unit-equivalence")
@@ -261,15 +253,13 @@ if __name__ == "__main__":
                       help="Select UGR/RGU unit-equivalence")
     umtx.add_argument('--rgu',     action='store_const', dest='unit', const='ugr',
                       help="Select UGR/RGU unit-equivalence")
-    umtx.add_argument('--version', type=float, choices=VERSIONS.keys(),
-                      help="Select ponderation version")
+    ugrp.add_argument('--ugr-version', type=str, default='1.0',
+                      choices=UGR_VERSIONS.keys(),
+                      help="Select UGR ponderation version")
     
     # Parse Arguments
     args = argp.parse_args(sys.argv[1:])
     args.unit = {'iguana': 'iguane', 'rgu': 'ugr'}.get(args.unit, args.unit)
-    if args.version:
-        args.unit = "version"
-    assert args.delimiter, "Delimiter cannot be empty string!"
     
     
     if   args.list_units:
@@ -291,6 +281,16 @@ if __name__ == "__main__":
         else:
             for k in gpus:
                 print(k)
+    elif args.list_ugr_versions:
+        ugr_versions = UGR_VERSIONS.keys()
+        if args.reverse:
+            ugr_versions = reversed(ugr_versions)
+        ugr_versions = {k:UGR_VERSIONS[k] for k in ugr_versions}
+        if args.json:
+            print(json.dumps(ugr_versions, indent=2))
+        else:
+            for k in ugr_versions.keys():
+                print(k) # Only print name, not weights data
     else:
         DATA = {}
         gpus = RAWDATA.keys()
@@ -309,5 +309,5 @@ if __name__ == "__main__":
                     print(f"{k}{args.delimiter}{DATA[k]}")
                 else:
                     print(f"{DATA[k]:-5.2f} {k}")
-        
+    
     sys.exit(0)
